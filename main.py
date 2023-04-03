705dca640776f665cbd328c500320521315cf50c
AntonShadrinNN <98612807+AntonShadrinNN@users.noreply.github.com>
2023-04-03 17:05:42 +0300

    implement model and helpful func TODO: Parsing data into json

--- main.py
+++ main.py
@@ -4,7 +4,68 @@ from deap import creator, base, tools
 from mutations.shuffle_mutation import mutate
 from crossover.random_crossover import crossover
 
-MUTATION_PROB = 0.85
+MUTATION_PROB = 0.05
+CROSSOVER_PROB = 0.9
+CAPACITY = 100
+
+
+def get_fitness(chr: list, cost: float):
+    return trucks_required(chr), get_cost(chr, cost)
+
+
+def get_cost(chr: list, cost: float) -> float:
+    total_cost = 0
+    routes = split_roads(chr)
+
+    for s_route in routes:
+        distance = 0
+        last_customer = 0
+
+        for c in s_route:
+            # TODO: find way to collect distance (distance matrix?)
+            d = 0
+
+            distance += d
+            last_customer = c
+
+        # TODO: add distance from last customer to depot
+        distance += 0
+
+        total_cost += cost * distance
+
+    return total_cost
+
+
+def split_roads(chr):
+    route: list[list] = []
+    sub_route = []
+    load = 0
+    last_customer = 0
+
+    for c in chr:
+        # TODO: find way to collect demand
+        demand = 0
+
+        new_load = load + demand
+
+        if new_load <= CAPACITY:
+            sub_route.append(c)
+            load = new_load
+        else:
+            route.append(sub_route)
+            sub_route = [c]
+            load = demand
+
+        last_customer = c
+
+    if sub_route:
+        route.append(sub_route)
+
+    return route
+
+
+def trucks_required(chr):
+    return len(split_roads(chr))
 
 
 class Vrp:
@@ -15,9 +76,6 @@ class Vrp:
         self.num_customers = num_customers
         self.population_size = population_size
         self.epochs = epochs
-        
-    def get_fitness(self, cost: float):
-        pass
 
     def define_model(self):
         # Create fitness and individual classes
@@ -33,7 +91,7 @@ class Vrp:
         self.tools.register('population', tools.initRepeat, list, self.tools.individual)
 
         # get individual fitness for chromosome
-        self.tools.register('get_fitness', self.get_fitness, cost=1.0)
+        self.tools.register('get_fitness', get_fitness, cost=1.0)
 
         # select ancestor for evolution
         self.tools.register('select', tools.selNSGA2)
@@ -57,6 +115,30 @@ class Vrp:
     def compile(self):
         for epoch in range(self.epochs):
             # select next generation
-            self.descendants = [tools. tools.selRoulette(self.population, len(self.population))]
+            self.descendants = [tools.clone(ind) for ind in tools.selRoulette(self.population, len(self.population))]
 
+            # loop for crossover and mutation operations
+            for chr1, chr2 in zip(self.descendants[::2], self.descendants[1::2]):
+                if random.random() < CROSSOVER_PROB:
+                    self.tools.crossover(chr1, chr2)
 
+                    del chr1.fitness.values, chr2.fitness.values
+
+                self.tools.mutate(chr1)
+                self.tools.mutate(chr2)
+
+            self.not_weighted = [ind for ind in self.descendants if not ind.fitness.valid]
+            self.fitnesses = self.tools.map(self.tools.fitness, self.not_weighted)
+
+            for chromosome, fit in zip(self.not_weighted, self.fitnesses):
+                chromosome.fitness.values = fit
+
+            self.population = self.tools.select(self.population + self.descendants, self.population_size)
+
+    def find_best(self):
+        self.best = tools.selBest(self.population, 1)[0]
+        print(f"Best individual is {self.best}")
+        print(f"Number of vechicles required are "
+              f"{self.best.fitness.values[0]}")
+        print(f"Cost required for the transportation is "
+              f"{self.best.fitness.values[1]}")
